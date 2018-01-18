0f1d258e07a01b1ef6aae36434b46f86bd0cc3bb
Heather Cape <heather.cape@verably.com>
2018-01-17 19:32:45 -0800

    Basic add, quote, buy, sell commands

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,22 +1,143 @@
-/*
-	Typify by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
+var Username;
 
-$("#get-quote").on("submit", function(e) {
+$("#set-user").on("submit", function(e) {
 	e.preventDefault();
-	console.log("worked");
-    $.ajax({
-        url:'/GetQuote',
-        type:'get',
-        data:$('#get-quote').serialize(),
-        success:function(){
-            //alert("worked");
-		}
+
+	var formData = {
+		'User'				 : $('input[name="User"').val()
+	};
+	console.log("Set");
+	Username = $('input[name="User"').val();
+
+	$.ajax({
+		url:'/AddUser',
+		type:'post',
+		dataType: "json",
+		contentType: 'application/json; charset=utf-8',
+		data: JSON.stringify(formData),
+		success:function(data){
+			console.log(data);
+			$('#current-user').replaceWith(document.createTextNode( "Logged in as : " + data.User ) );
+		},
+		error: function (xhr, status) {
+			alert("Sorry, there was a problem!");
+		},
 	});
 	return false;
 });
+
+
+$("#get-quote").on("submit", function(e) {
+	e.preventDefault();
+
+	var formData = {
+		'Stock'              : $('input[name="Stock"').val(),
+		'User'				 : Username
+	};
+
+	$.ajax({
+		url:'/GetQuote',
+		type:'post',
+		dataType: "json",
+		contentType: 'application/json; charset=utf-8',
+		data: JSON.stringify(formData),
+		success:function(data){
+			console.log(data);
+			$('#stock-price').html( data.Stock +":"+ data.Price );
+		},
+		error: function (xhr, status) {
+			alert("Sorry, there was a problem!");
+		},
+	});
+	return false;
+});
+
+
+$("#buy-stock").on("submit", function(e) {
+	e.preventDefault();
+
+	var formData = {
+		'User'				 : Username,
+		'Stock'				 : $('input[name="StockBuy"').val(),
+		'Amount'			 : Number($('input[name="AmountBuy"').val()),
+	};
+
+	$.ajax({
+		url:'/BuyStock',
+		type:'post',
+		dataType: "json",
+		contentType: 'application/json; charset=utf-8',
+		data: JSON.stringify(formData),
+		success:function(data){
+			console.log(data);
+			$('#confirm-buy').html( "Buy : " + data.Amount + "  Of : " + data.Stock );
+			var r = confirm("Buy stock?");
+				if (r == true) {
+					cancelorConfirm(data, '/CommitBuy', 'Commited');
+				} else {
+					cancelorConfirm(data, '/CancelBuy', 'Cancelled');
+				}
+		},
+		error: function (xhr, status) {
+			alert("Sorry, there was a problem!");
+		},
+	});
+	return false;
+});
+
+function cancelorConfirm(d, url, msg){
+	var formData = {
+		'User'				 : d.User
+	};
+	$.ajax({
+		url: url,
+		type:'post',
+		dataType: "json",
+		contentType: 'application/json; charset=utf-8',
+		data: JSON.stringify(d),
+		success:function(data){
+			console.log(data);
+			alert(msg);
+		},
+		error: function (xhr, status) {
+			alert("Sorry, there was a problem!");
+		},
+	});
+	return false;
+}
+
+$("#sell-stock").on("submit", function(e) {
+	e.preventDefault();
+
+	var formData = {
+		'User'				 : Username,
+		'Stock'				 : $('input[name="StockSell"').val(),
+		'Amount'			 : Number($('input[name="AmountSell"').val()),
+	};
+
+	$.ajax({
+		url:'/SellStock',
+		type:'post',
+		dataType: "json",
+		contentType: 'application/json; charset=utf-8',
+		data: JSON.stringify(formData),
+		success:function(data){
+			console.log(data);
+			$('#confirm-sell').html( "Sell : " + data.Amount + "  Of : " + data.Stock );
+			var r = confirm("Sell stock?");
+				if (r == true) {
+					cancelorConfirm(data, '/CommitSell', 'Commited');
+				} else {
+					cancelorConfirm(data, '/CancelSell', 'Cancelled');
+				}
+		},
+		error: function (xhr, status) {
+			alert("Sorry, there was a problem!");
+		},
+	});
+	return false;
+});
+
 
 (function($) {
 
--- server/server.go
+++ server/server.go
@@ -1,102 +1,300 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"regexp"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
+type GetQuote struct {
+	Stock string
+	User  string
 }
 
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
+type GetUser struct {
+	User string
 }
 
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
+type StockValue struct {
+	User   string
+	Stock  string
+	Amount int
+}
+
+type ReturnQuote struct {
+	Stock string
+	Price int
+}
+
+var stockName = regexp.MustCompile("([a-zA-Z][a-zA-Z][a-zA-Z])")
+
+func checkValidStockName(s string) bool {
+	m := stockName.FindStringSubmatch(s)
+	if m == nil {
+		return false
 	}
-	return &Page{Title: title, Body: body}, nil
+	return true
 }
 
-/* func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-		fmt.Println(err)
-		return
-	}
-	renderTemplate(w, "view", p)
-}
+func verifyGetUser(w http.ResponseWriter, r *http.Request, d *GetUser) bool {
 
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
-	}
-	renderTemplate(w, "edit", p)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
+	err := json.NewDecoder(r.Body).Decode(&d)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return false
 	}
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-} */
 
-//var templates = template.Must(template.ParseFiles("index.html"))
-
-/* func renderTemplate(w http.ResponseWriter, tmpl string) {
-	err := templates.ExecuteTemplate(w, tmpl+".html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-} */
-
-//var validPath = regexp.MustCompile("^/(edit|save|view)?(/)?([a-zA-Z0-9]*)$")
-
-var validPath = regexp.MustCompile("/")
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w, r)
-			return
+	if r.Method == "POST" {
+		if d.User == "" {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return false
 		}
-		fn(w, r, m[2])
 	}
+	return true
 }
 
-// AJAX Request Handler
+func verifyStockValueRequests(w http.ResponseWriter, r *http.Request, d *StockValue) bool {
+	err := json.NewDecoder(r.Body).Decode(&d)
+	if err != nil {
+		fmt.Println("yeet")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return false
+	}
+
+	if r.Method == "POST" {
+
+		pass := checkValidStockName(d.Stock)
+		if pass == false {
+			http.Error(w, "Wrong stock symbol", http.StatusInternalServerError)
+			return false
+		}
+
+		if d.Amount == 0 {
+			http.Error(w, "No money", http.StatusInternalServerError)
+			return false
+		}
+		if d.User == "" {
+			http.Error(w, "No User", http.StatusInternalServerError)
+			return false
+		}
+	}
+	return true
+}
+
+//add some error passing from TS
+func respondStockValueRequests(w http.ResponseWriter, d StockValue) {
+
+	response := StockValue{}
+	response.Stock = d.Stock
+	response.Amount = 123 //this amount will be rounded and returned in TS
+	response.User = d.User
+
+	responseJson, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(responseJson)
+}
+
 func quoteHandler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Println("method:", r.Method) //get request method
-	if r.Method == "GET" {
-		r.ParseForm()
-		// logic part of log in
-		fmt.Println("username:", r.Form["stock"])
+	var d GetQuote
+	err := json.NewDecoder(r.Body).Decode(&d)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+
+	if r.Method == "POST" {
+
+		pass := checkValidStockName(d.Stock)
+		if pass == false {
+			http.Error(w, "Wrong stock symbol", http.StatusInternalServerError)
+			return
+		}
+
+		if d.User == "" {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		response := ReturnQuote{}
+		response.Stock = d.Stock
+		response.Price = 123
+
+		responseJson, err := json.Marshal(response)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(responseJson)
+	}
+}
+
+func addUserHandler(w http.ResponseWriter, r *http.Request) {
+
+	var d GetUser
+	if !verifyGetUser(w, r, &d) {
+		return
+	}
+
+	//hit TS to check that user exists
+	//maybe send back if they are new or existing
+
+	response := GetUser{}
+	response.User = d.User
+
+	responseJson, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(responseJson)
+}
+
+func buyHandler(w http.ResponseWriter, r *http.Request) {
+
+	var d StockValue
+	if !verifyStockValueRequests(w, r, &d) {
+		return
+	}
+	//check user account
+
+	respondStockValueRequests(w, d)
+}
+
+func commitBuyHandler(w http.ResponseWriter, r *http.Request) {
+
+	var d GetUser
+	if !verifyGetUser(w, r, &d) {
+		return
+	}
+
+	//hit TS to check that user exists
+	//confirm buy etc
+	//not sure what sending back w yet
+
+	response := GetUser{}
+	response.User = d.User
+
+	responseJson, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(responseJson)
+}
+
+func cancelBuyHandler(w http.ResponseWriter, r *http.Request) {
+
+	var d GetUser
+	if !verifyGetUser(w, r, &d) {
+		return
+	}
+
+	//hit TS to check that user exists
+	//confirm buy etc
+	//not sure what sending back w yet
+
+	response := GetUser{}
+	response.User = d.User
+
+	responseJson, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(responseJson)
+}
+
+func sellHandler(w http.ResponseWriter, r *http.Request) {
+
+	var d StockValue
+	if !verifyStockValueRequests(w, r, &d) {
+		return
+	}
+
+	//check user account
+
+	respondStockValueRequests(w, d)
+}
+
+func commitSellHandler(w http.ResponseWriter, r *http.Request) {
+
+	var d GetUser
+	if !verifyGetUser(w, r, &d) {
+		return
+	}
+
+	//hit TS to check that user exists
+	//confirm buy etc
+	//not sure what sending back w yet
+
+	response := GetUser{}
+	response.User = d.User
+
+	responseJson, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(responseJson)
+}
+
+func cancelSellHandler(w http.ResponseWriter, r *http.Request) {
+
+	var d GetUser
+	if !verifyGetUser(w, r, &d) {
+		return
+	}
+
+	//hit TS to check that user exists
+	//confirm buy etc
+	//not sure what sending back w yet
+
+	response := GetUser{}
+	response.User = d.User
+
+	responseJson, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(responseJson)
+}
+
+func buyTriggerHandler(w http.ResponseWriter, r *http.Request) {
+
+	var d StockValue
+	if !verifyStockValueRequests(w, r, &d) {
+		return
+	}
+
+	//check user account
+
+	respondStockValueRequests(w, d)
 
 }
 
 func main() {
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("../frontend"))))
 	http.HandleFunc("/GetQuote", quoteHandler)
-	//http.HandleFunc("/view/", makeHandler(viewHandler))
-	//http.HandleFunc("/edit/", makeHandler(editHandler))
-	//http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/AddUser", addUserHandler)
+	http.HandleFunc("/BuyStock", buyHandler)
+	http.HandleFunc("/CommitBuy", commitBuyHandler)
+	http.HandleFunc("/CancelBuy", cancelBuyHandler)
+	http.HandleFunc("/CancelSell", cancelSellHandler)
+	http.HandleFunc("/CommitSell", commitSellHandler)
+	http.HandleFunc("/SellStock", buyHandler)
+	http.HandleFunc("/BuyStockTrigger", buyTriggerHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
